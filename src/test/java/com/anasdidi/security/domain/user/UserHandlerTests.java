094badc0f530013a289ee85c23f61287e8a2ae28
Anas Juwaidi <anas.didi95@gmail.com>
2021-05-31 20:06:58 +0800

    Add test

--- src/test/java/com/anasdidi/security/domain/user/UserHandlerTests.java
+++ src/test/java/com/anasdidi/security/domain/user/UserHandlerTests.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
@@ -23,22 +24,38 @@ public class UserHandlerTests {
     this.webTestClient = webTestClient;
   }
 
+  private Map<String, Object> generateUserMap() {
+    String suffix = "" + System.currentTimeMillis();
+    Map<String, Object> map = new HashMap<>();
+    map.put("username", "username" + suffix);
+    map.put("password", "password" + suffix);
+    map.put("fullName", "fullName" + suffix);
+    map.put("email", "email" + suffix);
+
+    return map;
+  }
+
   @Test
   public void testUserCreateSuccess() {
-    String suffix = "" + System.currentTimeMillis();
-    Map<String, Object> requestBody = new HashMap<>();
-    requestBody.put("username", "username" + suffix);
-    requestBody.put("password", "password" + suffix);
-    requestBody.put("fullName", "fullName" + suffix);
-    requestBody.put("email", "email" + suffix);
+    Map<String, Object> requestBody = generateUserMap();
 
     ResponseSpec response = webTestClient.post().uri("/user").accept(MediaType.APPLICATION_JSON)
         .contentType(MediaType.APPLICATION_JSON).bodyValue(requestBody).exchange();
-    response.expectStatus().isCreated();
+    response.expectStatus().isEqualTo(HttpStatus.CREATED);
 
     @SuppressWarnings("unchecked")
     Map<String, Object> responseBody =
         response.expectBody(Map.class).returnResult().getResponseBody();
     Assertions.assertNotNull(responseBody.get("id"));
+  }
+
+  @Test
+  public void testUserCreateValidationError() {
+    Map<String, Object> requestBody = generateUserMap();
+    requestBody.put("username", null);
+
+    ResponseSpec response = webTestClient.post().uri("/user").accept(MediaType.APPLICATION_JSON)
+        .contentType(MediaType.APPLICATION_JSON).bodyValue(requestBody).exchange();
+    response.expectStatus().isEqualTo(HttpStatus.BAD_REQUEST);
   }
 }
