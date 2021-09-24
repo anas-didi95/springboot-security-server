f5abaf5f1f36964343708662bc342195fad67d2e
Anas Juwaidi <anas.didi95@gmail.com>
2021-09-24 21:05:08 +0800

    ADD: Add library and utils to handle jwt token

--- src/main/java/com/anasdidi/security/domain/auth/AuthServiceBean.java
+++ src/main/java/com/anasdidi/security/domain/auth/AuthServiceBean.java
@@ -1,11 +1,14 @@
 package com.anasdidi.security.domain.auth;
 
+import java.util.Collection;
 import java.util.List;
 import com.anasdidi.security.domain.user.UserRepository;
 import com.anasdidi.security.domain.user.UserVO;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
@@ -32,14 +35,62 @@ class AuthServiceBean implements AuthService {
       List<UserVO> resultList = userRepository.findByUsername(dto.username);
 
       if (logger.isDebugEnabled()) {
-        logger.debug("[{}:{}] resultList.size=", TAG, dto.sessionId,
-            (resultList != null ? resultList.size() : -1));
+        logger.debug("[{}:{}] resultList.size=", TAG, dto.sessionId, (resultList != null ? resultList.size() : -1));
       }
 
       if (resultList != null && !resultList.isEmpty()) {
         UserVO vo = resultList.get(0);
         if (vo.getPassword().equals(dto.password)) {
-          return Mono.just("" + System.currentTimeMillis());
+          String accessToken = AuthUtils.generateToken(new UserDetails() {
+
+            @Override
+            public Collection<? extends GrantedAuthority> getAuthorities() {
+              // TODO Auto-generated method stub
+              return null;
+            }
+
+            @Override
+            public String getPassword() {
+              // TODO Auto-generated method stub
+              return null;
+            }
+
+            @Override
+            public String getUsername() {
+              // TODO Auto-generated method stub
+              return null;
+            }
+
+            @Override
+            public boolean isAccountNonExpired() {
+              // TODO Auto-generated method stub
+              return false;
+            }
+
+            @Override
+            public boolean isAccountNonLocked() {
+              // TODO Auto-generated method stub
+              return false;
+            }
+
+            @Override
+            public boolean isCredentialsNonExpired() {
+              // TODO Auto-generated method stub
+              return false;
+            }
+
+            @Override
+            public boolean isEnabled() {
+              // TODO Auto-generated method stub
+              return false;
+            }
+          });
+
+          if (logger.isDebugEnabled()) {
+            logger.debug("[login:{}] accessToken={}", dto.sessionId, accessToken);
+          }
+
+          return Mono.just(accessToken);
         }
       }
 
