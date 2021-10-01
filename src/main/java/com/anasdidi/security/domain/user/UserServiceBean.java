e44f66c11a8ad006a6148368aee96c817a68ac47
Anas Juwaidi <anas.didi95@gmail.com>
2021-10-01 20:36:00 +0800

    ADD: Implement encode password during user creation

--- src/main/java/com/anasdidi/security/domain/user/UserServiceBean.java
+++ src/main/java/com/anasdidi/security/domain/user/UserServiceBean.java
@@ -2,13 +2,17 @@ package com.anasdidi.security.domain.user;
 
 import java.util.Date;
 import java.util.Optional;
-import java.util.UUID;
+
 import com.anasdidi.security.common.ApplicationException;
 import com.anasdidi.security.common.ApplicationMessage;
+import com.anasdidi.security.common.ApplicationUtils;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
 import reactor.core.publisher.Mono;
 
 @Service
@@ -17,30 +21,26 @@ class UserServiceBean implements UserService {
   private static final Logger logger = LogManager.getLogger(UserServiceBean.class);
   private final UserRepository userRepository;
   private final ApplicationMessage message;
+  private final BCryptPasswordEncoder passwordEncoder;
 
   @Autowired
-  UserServiceBean(UserRepository userRepository, ApplicationMessage message) {
+  UserServiceBean(UserRepository userRepository, ApplicationMessage message, BCryptPasswordEncoder passwordEncoder) {
     this.userRepository = userRepository;
     this.message = message;
+    this.passwordEncoder = passwordEncoder;
   }
 
   @Override
   public Mono<String> create(UserDTO dto) {
-    final String TAG = "create";
-
-    if (logger.isDebugEnabled()) {
-      logger.debug("[{}:{}] {}", TAG, dto.sessionId, dto);
-    }
-
     return Mono.just(dto.toVO()).map(vo -> {
-      String id = UUID.randomUUID().toString().replaceAll("-", "").toUpperCase();
-      vo.setId(id);
+      vo.setId(ApplicationUtils.getFormattedUUID());
+      vo.setPassword(passwordEncoder.encode(dto.password));
       vo.setLastModifiedDate(new Date());
       vo.setVersion(0);
       return vo;
     }).map(vo -> userRepository.save(vo)).doOnError(e -> {
-      logger.error("[{}:{}] {}", TAG, dto.sessionId, e.getMessage());
-      logger.error("[{}:{}] {}", TAG, dto.sessionId, dto);
+      logger.error("[create:{}] {}", dto.sessionId, e.getMessage());
+      logger.error("[create:{}] {}", dto.sessionId, dto);
       e.addSuppressed(new ApplicationException(UserConstants.ERROR_CREATE,
           message.getErrorMessage(UserConstants.ERROR_CREATE), e.getMessage()));
     }).map(vo -> vo.getId());
@@ -48,12 +48,6 @@ class UserServiceBean implements UserService {
 
   @Override
   public Mono<String> update(UserDTO dto) {
-    final String TAG = "update";
-
-    if (logger.isDebugEnabled()) {
-      logger.debug("[{}:{}] {}", TAG, dto.sessionId, dto);
-    }
-
     Mono<UserVO> db = Mono.defer(() -> {
       Optional<UserVO> result = userRepository.findById(dto.id);
 
@@ -62,11 +56,11 @@ class UserServiceBean implements UserService {
         if (vo.getVersion() == dto.version) {
           return Mono.just(vo);
         } else {
-          logger.error("[{}:{}] {}", TAG, dto.sessionId, dto);
+          logger.error("[update:{}] {}", dto.sessionId, dto);
           return Mono.error(UserException.getVersionNotMatched(message, vo));
         }
       } else {
-        logger.error("[{}:{}] {}", TAG, dto.sessionId, dto);
+        logger.error("[update:{}] {}", dto.sessionId, dto);
         return Mono.error(UserException.getUserNotFound(message, dto));
       }
     });
@@ -82,12 +76,6 @@ class UserServiceBean implements UserService {
 
   @Override
   public Mono<String> delete(UserDTO dto) {
-    final String TAG = "delete";
-
-    if (logger.isDebugEnabled()) {
-      logger.debug("[{}:{}] {}", TAG, dto.sessionId, dto);
-    }
-
     return Mono.defer(() -> {
       Optional<UserVO> userVO = userRepository.findById(dto.id);
 
@@ -96,11 +84,11 @@ class UserServiceBean implements UserService {
         if (vo.getVersion() == dto.version) {
           return Mono.just(userVO.get());
         } else {
-          logger.error("[{}:{}] {}", TAG, dto.sessionId, dto);
+          logger.error("[delete:{}", dto.sessionId, dto);
           return Mono.error(UserException.getVersionNotMatched(message, vo));
         }
       } else {
-        logger.error("[{}:{}] {}", TAG, dto.sessionId, dto);
+        logger.error("[delete:{}] {}", dto.sessionId, dto);
         return Mono.error(UserException.getUserNotFound(message, dto));
       }
     }).map(vo -> {
