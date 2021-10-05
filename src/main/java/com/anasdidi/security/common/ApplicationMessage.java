8473f02fd41c0ea492ec54acb3ba6efc2f767a26
Anas Juwaidi <anas.didi95@gmail.com>
2021-10-05 20:43:01 +0800

    MOD: Amend class streotype

--- src/main/java/com/anasdidi/security/common/ApplicationMessage.java
+++ src/main/java/com/anasdidi/security/common/ApplicationMessage.java
@@ -1,11 +1,11 @@
 package com.anasdidi.security.common;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
 
-@Configuration
+@Component
 @PropertySource(value = "classpath:message.properties")
 public class ApplicationMessage {
 
