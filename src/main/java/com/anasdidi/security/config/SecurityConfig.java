333c7029913222457ed09a76acf845f750ecca79
Anas Juwaidi <anas.didi95@gmail.com>
2021-10-22 12:18:10 +0800

    MOD: Configure security config for graphql
    If graphiql enable, the security config for graphql-related endpoints
    are disable. Thus, ensure the graphiql only enable when necessary,
    and disable when in production.
    
    The CSRF protection need disable to ensure graphiql can send request.
    Might need to find out why if need CSRF protection enable in future.

--- src/main/java/com/anasdidi/security/config/SecurityConfig.java
+++ src/main/java/com/anasdidi/security/config/SecurityConfig.java
@@ -1,8 +1,11 @@
 package com.anasdidi.security.config;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import com.anasdidi.security.filter.RequestTraceIdFilter;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
@@ -11,7 +14,6 @@ import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-
 import reactor.core.publisher.Mono;
 
 @Configuration
@@ -21,31 +23,38 @@ public class SecurityConfig {
   private final AuthenticationManager authenticationManager;
   private final SecurityContextRepository securityContextRepository;
   private final RequestTraceIdFilter requestTraceIdFilter;
+  private final String GRAPHIQL_ENABLE;
 
   @Autowired
   public SecurityConfig(AuthenticationManager authenticationManager,
       SecurityContextRepository securityContextRepository,
-      RequestTraceIdFilter requestTraceIdFilter) {
+      RequestTraceIdFilter requestTraceIdFilter,
+      @Value("${graphiql.enabled}") String GRAPHIQL_ENABLE) {
     this.authenticationManager = authenticationManager;
     this.securityContextRepository = securityContextRepository;
     this.requestTraceIdFilter = requestTraceIdFilter;
+    this.GRAPHIQL_ENABLE = GRAPHIQL_ENABLE;
   }
 
   @Bean
   public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
-    String[] patterns = new String[] {"/auth/login/**"};
+    List<String> patterns = new ArrayList<>(Arrays.asList("/auth/login/**"));
 
-    return http.csrf().disable().cors().disable().authorizeExchange().anyExchange().permitAll()
-        .and().build();
-    // return http.cors().disable().exceptionHandling()
-    // .authenticationEntryPoint((exchange, exception) -> Mono.fromRunnable(() -> {
-    // exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
-    // })).accessDeniedHandler((exchange, exception) -> Mono.fromRunnable(() -> {
-    // exchange.getResponse().setStatusCode(HttpStatus.FORBIDDEN);
-    // })).and().csrf().disable().authenticationManager(authenticationManager)
-    // .securityContextRepository(securityContextRepository).authorizeExchange().pathMatchers(patterns).permitAll()
-    // .anyExchange().authenticated().and()
-    // .addFilterBefore(requestTraceIdFilter, SecurityWebFiltersOrder.AUTHENTICATION).build();
+    if (Boolean.parseBoolean(GRAPHIQL_ENABLE)) {
+      patterns.addAll(
+          Arrays.asList("/graphiql**", "/vendor/graphiql/**", "/graphql**", "/subscriptions**"));
+    }
+
+    return http.csrf().disable().cors().disable().exceptionHandling()
+        .authenticationEntryPoint((exchange, exception) -> Mono.fromRunnable(() -> {
+          exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+        })).accessDeniedHandler((exchange, exception) -> Mono.fromRunnable(() -> {
+          exchange.getResponse().setStatusCode(HttpStatus.FORBIDDEN);
+        })).and().csrf().disable().authenticationManager(authenticationManager)
+        .securityContextRepository(securityContextRepository).authorizeExchange()
+        .pathMatchers(patterns.toArray(new String[] {})).permitAll().anyExchange().authenticated()
+        .and().addFilterBefore(requestTraceIdFilter, SecurityWebFiltersOrder.AUTHENTICATION)
+        .build();
   }
 
   @Bean
