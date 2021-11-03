59c24c55a0c15ce44c63085a02ca6dcd65a62296
Anas Juwaidi <anas.didi95@gmail.com>
2021-11-03 16:59:20 +0800

    REF: Refactor configuration file
    Remove redundant enable annotations. The annotations are handled
    by spring boot auto-configure.

--- src/main/java/com/anasdidi/security/config/DatabaseConfig.java
+++ src/main/java/com/anasdidi/security/config/DatabaseConfig.java
@@ -4,19 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.ReactiveAuditorAware;
 import org.springframework.data.r2dbc.config.EnableR2dbcAuditing;
-import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
-import org.springframework.web.reactive.config.PathMatchConfigurer;
-import org.springframework.web.reactive.config.WebFluxConfigurer;
 
 @Configuration
-@EnableR2dbcRepositories
 @EnableR2dbcAuditing(auditorAwareRef = "auditorAware")
-public class AppConfig implements WebFluxConfigurer {
-
-  @Override
-  public void configurePathMatching(PathMatchConfigurer configurer) {
-    WebFluxConfigurer.super.configurePathMatching(configurer);
-  }
+public class DatabaseConfig {
 
   @Bean
   public ReactiveAuditorAware<String> auditorAware() {
